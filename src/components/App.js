124424b1abbc004baa14b5bc0e6bd43f011193ff
Shubham  Aher <84302530+Shubhamaher1@users.noreply.github.com>
2022-02-24 10:20:52 +0530

    re

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,17 @@ import "../styles/App.css";
 const App = (props) => {
   const [index, setIndex] = useState(0);
   const [data, setdata] = useState(props.slides[2]);
-  console.log(props.slides[3]);
-  console.log(props.slides);
+
   return (
     <>
-      <h1 data-testid="title">{data.title}</h1>
-      <p data-testid="text">{data.text}</p>
+      <h1 data-testid="title">{props.slides[index].title}</h1>
+      <p data-testid="text">{props.slides[index].text}</p>
       {index === 0 ? null : (
         <button
           data-testid="button-prev"
           onClick={() => {
             setIndex(index - 1);
-            setdata(props.slides[index]);
+            // setdata(props.slides[index]);
           }}
         >
           prev
@@ -26,21 +25,23 @@ const App = (props) => {
           data-testid="button-next"
           onClick={() => {
             setIndex(index + 1);
-            setdata(props.slides[index]);
+            // setdata(props.slides[index]);
           }}
         >
           next
         </button>
       )}
-      <button
-        data-testid="button-restart"
-        onClick={() => {
-          setIndex(0);
-          setdata(props.slides[index]);
-        }}
-      >
-        restart
-      </button>
+      {index === 0 ? null : (
+        <button
+          data-testid="button-restart"
+          onClick={() => {
+            setIndex(0);
+            // setdata(props.slides[index]);
+          }}
+        >
+          restart
+        </button>
+      )}
     </>
   );
 };
