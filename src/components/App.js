8c75d81198e5d2fa9c9b192ab90a620e3c3246bd
Shubham  Aher <84302530+Shubhamaher1@users.noreply.github.com>
2022-02-24 10:11:45 +0530

    er

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,48 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-const App = () => {
+const App = (props) => {
+  const [index, setIndex] = useState(0);
+  const [data, setdata] = useState(props.slides[2]);
+  console.log(props.slides[3]);
+  console.log(props.slides);
   return (
-    <></>
-  )
-}
-
+    <>
+      <h1 data-testid="title">{data.title}</h1>
+      <p data-testid="text">{data.text}</p>
+      {index === 0 ? null : (
+        <button
+          data-testid="button-prev"
+          onClick={() => {
+            setIndex(index - 1);
+            setdata(props.slides[index]);
+          }}
+        >
+          prev
+        </button>
+      )}
+      {index === 4 ? null : (
+        <button
+          data-testid="button-next"
+          onClick={() => {
+            setIndex(index + 1);
+            setdata(props.slides[index]);
+          }}
+        >
+          next
+        </button>
+      )}
+      <button
+        data-testid="button-restart"
+        onClick={() => {
+          setIndex(0);
+          setdata(props.slides[index]);
+        }}
+      >
+        restart
+      </button>
+    </>
+  );
+};
 
 export default App;
