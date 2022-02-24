6272c388fae125de632dfd29b5e09dc5b90195cf
Shubham  Aher <84302530+Shubhamaher1@users.noreply.github.com>
2022-02-24 10:36:53 +0530

    sd

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,18 @@ const App = (props) => {
     <>
       <h1 data-testid="title">{props.slides[index].title}</h1>
       <p data-testid="text">{props.slides[index].text}</p>
-      {index === 0 ? null : (
+      {index === 0 ? (
+        <button
+          data-testid="button-prev"
+          style={{ display: "none" }}
+          onClick={() => {
+            setIndex(index - 1);
+            // setdata(props.slides[index]);
+          }}
+        >
+          Prev
+        </button>
+      ) : (
         <button
           data-testid="button-prev"
           onClick={() => {
@@ -20,7 +31,18 @@ const App = (props) => {
           Prev
         </button>
       )}
-      {index === 4 ? null : (
+      {index === 4 ? (
+        <button
+          data-testid="button-next"
+          style={{ display: "none" }}
+          onClick={() => {
+            setIndex(index + 1);
+            // setdata(props.slides[index]);
+          }}
+        >
+          Next
+        </button>
+      ) : (
         <button
           data-testid="button-next"
           onClick={() => {
